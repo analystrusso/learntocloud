205918812aaf997d68d65e87231057f1caa675b0
analystrusso <adam.jordan.russo@gmail.com>
2024-04-06 10:09:58 -0400

    commit

--- clouduploader.sh
+++ clouduploader.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "hello world"
+# set name of bucket
+bucket_name="clouduploaderbucket"
+
+# set the sourcedirectory
+sourcedir="C:\Users\adamj\Documents\LearntoCloud\learntocloud\sourcedir"
+
+# copy files to s3 bucket
+aws s3 cp "$sourcedir" "s3://$bucket_name/" --recursive
+
+#check if copy was successful
+if [ $? -eq 0 ]; then
+	echo "File or directory uploaded successfully to $bucket_name."
+else 
+	echo "Failed to upload file or directory to $bucket_name."
+fi
